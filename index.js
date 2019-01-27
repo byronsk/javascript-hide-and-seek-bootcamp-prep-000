a8e1a189f1c7495f5d92cc17c244c9b7b35d35eb
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-27 19:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget() {
+return document.getElementById('nested').querySelector('.target');
+}
+
+
+var deepestLevel = 0;
+var deepestLevelText = "";
+
+function deepestChild() {
+  var nodes = document.querySelectorAll('#grand-node div');
+  if(!nodes.length) 
+  return false;
+  return nodes[nodes.length-1];
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n;
+  }
+}
